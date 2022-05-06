c9af3d59fe9b9cd38a5c5b54c88f2b184bf4b81f
Jérémy <jeremyschneider21@gmail.com>
2022-05-06 14:51:45 +0200

    Add profile and follow models

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    # username = models.CharField(max_length=20, null=False, unique=True)
-    # email = models.EmailField(null=False, unique=True)
-    # firstname = models.TextField(null=False)
-    # lastname = models.TextField(null=False)
     country = models.TextField(blank=True)
     display_name = models.CharField(max_length=50, null=False)
-    # created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+class Profile(models.Model):
+    id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    bio = models.CharField(max_length=255, blank=True)
+    url = models.JSONField(blank=True)
+    picture = models.URLField()
+
+class Follow(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    following_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    followed_at = models.DateTimeField(auto_now_add=True, blank=True)
